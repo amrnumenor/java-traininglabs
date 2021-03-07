82f1449041f47ab038e88af069db55ca8b196c29
mobius-96 <yuenhern96@gmail.com>
2021-03-07 18:26:38 +0800

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+Hands-on for Java Basics - WIP
+
+Note: To be used together with accompanying slides in Mastering AI for Beginners curriculum.
---
+++ src/main/java/global/skymind/TestRunApp.java
@@ -0,0 +1,13 @@
+package global.skymind;
+
+/**
+ * Hello Skymind!
+ *
+ */
+public class TestRunApp
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello Skymind!" );
+    }
+}
